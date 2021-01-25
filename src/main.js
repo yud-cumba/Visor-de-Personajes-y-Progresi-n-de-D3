33f806598a31604751ec78d58fbad8687d049ac6
yud-cumba <yudith@MacBook-Air-de-Yud.local>
2021-01-24 22:59:57 -0500

    feat: NavBar y Footer

--- src/main.js
+++ src/main.js
@@ -9,6 +9,30 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './assets/css/main.styl'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// Iconos de tipo "Solid"
+import { faSkull, faCrown, faDungeon, faHatWizard, faHammer, faGem } from '@fortawesome/free-solid-svg-icons'
+// Iconos de tipo "Brand" (marcas o logos de empresas)
+import { faVuejs, faBootstrap, faFontAwesome, faGithub, faBattleNet } from '@fortawesome/free-brands-svg-icons'
+// El componente que vamos a utilizar
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Le añadimos los iconos que acabamos de importar (todos, los de tipo solid y de tipo brand)
+library.add(
+  faSkull,
+  faCrown,
+  faDungeon,
+  faHatWizard,
+  faHammer,
+  faGem,
+  faVuejs,
+  faBootstrap,
+  faFontAwesome,
+  faGithub,
+  faBattleNet
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 // Darlo de alta
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
