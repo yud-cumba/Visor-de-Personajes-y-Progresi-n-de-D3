7ec119641514a68e5a32e8bee627b0b0ef60f527
yud-cumba <yudith@MacBook-Air-de-Yud.local>
2021-01-28 20:31:13 -0500

    Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,56 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home/Index.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About/Index.vue')
-  }
+// Configuración rutas
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+  { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+  { path: '/about', name: 'About' },
+  { path: '/error', name: 'Error' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
 
-const router = new VueRouter({
+// Rutas
+const routes = routerOptions.map(r => {
+  return {
+    ...r,
+    // Lazy load
+    component: () => import(/* webpackChunkName: "[request]" */`@/views/${r.name}/Index.vue`)
+  }
+})
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Home',
+//     component: () => import(/* webpackChunkName: "Home" */ '@/views/Home/Index.vue')
+//   },
+//   {
+//     path: '/about',
+//     name: 'About',
+//     component: () => import(/* webpackChunkName: "About" */ '@/views/About/Index.vue')
+//   },
+//   {
+//     path: '/region/:region/profile/:battleTag',
+//     name: 'Profile',
+//     component: () => import(/* webpackChunkName: "Profile" */ '@/views/Profile/Index.vue')
+//   },
+//   {
+//     path: '/region/:region/profile/:battleTag/hero/:heroId',
+//     name: 'Hero',
+//     component: () => import(/* webpackChunkName: "Hero" */ '@/views/Hero/Index.vue')
+//   },
+//   {
+//     path: '/error',
+//     name: 'Error',
+//     component: () => import(/* webpackChunkName: "Error" */ '@/views/Error/Index.vue')
+//   }
+// ]
+
+const router = new Router({
   routes
 })
 
