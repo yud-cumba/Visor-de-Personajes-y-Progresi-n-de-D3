20a00fbb0c325098bc521250bba32616d8303c4e
yud-cumba <yudith.cumba@gmail.com>
2021-02-08 09:24:22 -0500

    fix: cambio de sensitive vista

--- src/App.vue
+++ src/App.vue
@@ -34,6 +34,6 @@ export default {
     font-family 'Avenir', Helvetica, Arial, sans-serif
     -webkit-font-smoothing antialiased
     -moz-osx-font-smoothing grayscale
-    color #ffffff // Le ponemos un color de letra blanco para que resalte
-    background-color #15202b // Le cambiamos el color de fondo por un azul marino oscuro
+    color #ffffff
+    background-color #15202b
 </style>
