d3d553e8527ebc02f6518d0494b053cb8b5884d3
yud-cumba <yudith.cumba@gmail.com>
2021-02-08 14:52:15 -0500

    feat: Configuracion nombre original

--- src/views/Home/HomeTitle.vue
+++ src/views/Home/HomeTitle.vue
@@ -2,11 +2,11 @@
   <div class="home-title text-center">
     <!-- <h1 class="my-5 font-diablo">Diablo 3 Profile Finder</h1> -->
     <h1 v-diablo:white.bottom class="my-5">Diablo 3 Profile Finder</h1>
-    <p class="lead text-muted">Coloca tu
+    <p class="lead text-muted">Write your
       <em>
         <a href="https://eu.battle.net/support/es/article/75767" target="_blank" title="Format: YourProfile#1234">battle-tag</a>
       </em>
-      y selecciona tu región para ver tu perfil</p>
+      and choose your region profile.</p>
     <hr class="mt-5">
   </div>
 </template>
